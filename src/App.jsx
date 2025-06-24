d1a3c602bb859d2d1b81964bc1f577c4f8bd1dd7
Khanh <khanh@starchdev.com>
2025-06-24 01:17:02 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./App.css";
+import BaristaForm from "./components/BaristaForm";
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="title-container">
+                <h1 className="title">On My Grind</h1>
+                <p>
+                    So you think you can barista? Let's put that to the test...
+                </p>
+            </div>
+            <BaristaForm />
+        </div>
+    );
+};
+
+export default App;
